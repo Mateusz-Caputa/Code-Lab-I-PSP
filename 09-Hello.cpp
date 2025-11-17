5b465d5fc074e32d9a0e4baa63e57ff5e2734cc4
Mateusz-Caputa <mat.caputa@outlook.com>
2025-11-17 22:57:00 +0000

    Complete exercise 9.

---
+++ 09-Hello.cpp
@@ -0,0 +1,20 @@
+/* File: 09-Hello.cpp
+ * - The program outputs "Hello" by calling hello function.
+ */
+#include <iostream>
+using namespace std;
+
+// hello function definition
+void hello()
+{
+    cout << "Hello" << endl; // output hello
+}
+
+// main function definition
+int main()
+{
+    // invoke hello function
+    hello();
+
+    return 0; // return 0 status code to system
+}
